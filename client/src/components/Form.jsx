0bfbbd6d48f9dbe2a42b5d2f9a66cbd8bf3ee3c3
LK152 <wuluke0@gmail.com>
2021-12-06 13:12:12 +0800

    Update Form.jsx

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 import {
-	Box,
-	Button,
-	TextField,
-	InputLabel,
-	FormControl,
-	FormHelperText,
-	styled,
-	IconButton,
-	Typography,
+    Box,
+    Button,
+    TextField,
+    InputLabel,
+    FormControl,
+    FormHelperText,
+    styled,
+    IconButton,
+    Typography,
 } from '@mui/material';
 import { Send, DeleteOutline } from '@mui/icons-material';
 import { mainTopics, subTopics, members } from './Options';
@@ -17,192 +17,215 @@ import StudentIdValidator from '../validator/StudentIdValidator';
 import '../App.css';
 
 const initialValues = {
-	studentId: '',
-	mainTopic: '',
-	subTopic: '',
-	otherTopic: '',
-	isSent: false,
-	members: '',
+    studentId: '',
+    mainTopic: '',
+    subTopic: '',
+    otherTopic: '',
+    isSent: false,
+    members: '',
+    memberInfo1: '',
+    memberInfo2: '',
 };
 
 const Submitbtn = styled(Button)({
-	border: '1px solid rgb(25, 118, 210)',
-	boxSizing: 'border-box',
-	transition: '.5s ease-in-out',
-	overflow: 'hidden',
-	'&:before': {
-		content: '""',
-		borderRight: '50px solid transparent',
-		borderTop: '36px solid rgb(25, 118, 210)',
-		transform: 'translateX(-100%)',
-		position: 'absolute',
-		top: 0,
-		bottom: 0,
-		right: -50,
-		left: 0,
-		transition: '.5s ease-in-out',
-	},
-	'&:hover': {
-		color: '#FFF',
-		'&:before': {
-			transform: 'translateX(0)',
-			transition: '.5s ease-in-out',
-		},
-	},
+    border: '1px solid rgb(25, 118, 210)',
+    boxSizing: 'border-box',
+    transition: '.5s ease-in-out',
+    overflow: 'hidden',
+    '&:before': {
+        content: '""',
+        borderRight: '50px solid transparent',
+        borderTop: '36px solid rgb(25, 118, 210)',
+        transform: 'translateX(-100%)',
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        right: -50,
+        left: 0,
+        transition: '.5s ease-in-out',
+    },
+    '&:hover': {
+        color: '#FFF',
+        '&:before': {
+            transform: 'translateX(0)',
+            transition: '.5s ease-in-out',
+        },
+    },
 });
 
 const Form = () => {
-	const [values, setValues] = useState(initialValues);
+    const [values, setValues] = useState(initialValues);
 
-	const handleTextChange = (e) => {
-		setValues({ ...values, [e.target.name]: e.target.value });
-	};
+    const handleTextChange = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+    };
 
-	const handleTopicChange = (value) => {
-		setValues({ ...values, mainTopic: value });
-	};
+    const handleTopicChange = (value) => {
+        setValues({ ...values, mainTopic: value });
+    };
 
-	const handleSubTopicChange = (value) => {
-		setValues({ ...values, subTopic: value });
-	};
+    const handleSubTopicChange = (value) => {
+        setValues({ ...values, subTopic: value });
+    };
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		setValues({ ...values, isSent: true });
-	};
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setValues({ ...values, isSent: true });
+    };
 
-	const handleDelete = () => {
-		setValues(initialValues);
-	};
+    const handleDelete = () => {
+        setValues(initialValues);
+    };
 
-	const handleMemberSelect = (value) => {
-		setValues({ ...values, members: value });
-	};
+    const handleMemberSelect = (value) => {
+        setValues({ ...values, members: value });
+    };
 
-	const memberInput = (num) => {
-		let fields = [];
-		for (var i = 0;i < num;i++) {
-			fields.push(<TextField label={"學號" + (i+1)} />);
-		};
-		return (
-			<Box sx={{ my: { xs: 3 }, display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-				{fields}
-			</Box>
-		);
-	}
+    const memberInput = (num) => {
+        let fields = [];
+        for (var i = 1; i < num; i++) {
+            fields.push(
+                <FormControl fullWidth>
+                    <TextField
+                        label={'學生' + i}
+                        value={
+                            i === 1 ? values.memberInfo1 : values.memberInfo2
+                        }
+                        onChange={handleTextChange}
+                        name={'memberInfo' + i}
+                    />
+                    <FormHelperText>輸入學生班級座號</FormHelperText>
+                </FormControl>
+            );
+        }
+        return (
+            <Box
+                sx={{
+                    my: { xs: 3 },
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                }}
+            >
+                {fields}
+            </Box>
+        );
+    };
 
-	switch (values.isSent) {
-		case true:
-			return (
-				<Typography variant='h1' align='center'>
-					已送出
-				</Typography>
-			);
+    switch (values.isSent) {
+        case true:
+            return (
+                <Typography variant="h1" align="center">
+                    已送出
+                </Typography>
+            );
 
-		default:
-			return (
-				<form onSubmit={handleSubmit}>
-					<Box sx={{ my: { xs: 3 } }}>
-						<FormControl fullWidth>
-							<TextField
-								variant='filled'
-								label='學號'
-								name='studentId'
-								color={
-									StudentIdValidator(values.studentId)
-										? 'success'
-										: 'warning'
-								}
-								value={values.studentId}
-								onChange={handleTextChange}
-							/>
-							{!StudentIdValidator(values.studentId) && (
-								<FormHelperText>請輸入8碼學號</FormHelperText>
-							)}
-						</FormControl>
-					</Box>
-					{StudentIdValidator(values.studentId) && (
-						<Box sx={{ my: { xs: 3 } }}>
-							<FormControl fullWidth>
-								<InputLabel>主題 *</InputLabel>
-								<Select
-									label='主題 *'
-									name='mainTopic'
-									options={mainTopics}
-									value={values.mainTopic}
-									onChange={handleTopicChange}
-								/>
-							</FormControl>
-						</Box>
-					)}
-					{values.mainTopic !== '' && values.mainTopic !== 8 && (
-						<Box sx={{ my: { xs: 3 } }}>
-							<FormControl fullWidth>
-								<InputLabel>副主題 *</InputLabel>
-								<Select
-									label='副主題 *'
-									name='subTopic'
-									options={subTopics[values.mainTopic]}
-									value={values.subTopic}
-									onChange={handleSubTopicChange}
-								/>
-							</FormControl>
-						</Box>
-					)}
-					{values.mainTopic === 8 && (
-						<Box sx={{ my: { xs: 3 } }}>
-							<FormControl fullWidth>
-								<TextField
-									variant='filled'
-									label='其他'
-									name='otherTopic'
-									value={values.otherTopic}
-									onChange={handleTextChange}
-								/>
-							</FormControl>
-						</Box>
-					)}
-					{console.log(values.members)}
-					{(values.subTopic !== '' || values.otherTopic !== '') && (
-						<Box sx={{ my: { xs: 3 } }}>
-							<FormControl fullWidth>
-								<InputLabel>組員人數</InputLabel>
-								<Select
-									label='組員人數'
-									name='members'
-									options={members}
-									value={values.members}
-									onChange={handleMemberSelect}
-								/>
-							</FormControl>
-						</Box>
-					)}
-					{values.members !== 1 && (
-						<>
-							{memberInput(values.members)}
-						</>
-					)}
-					<IconButton onClick={handleDelete}>
-						<DeleteOutline />
-					</IconButton>
-					<Submitbtn
-						type='submit'
-						disableRipple
-						disabled={
-							values.mainTopic === 8
-								? values.otherTopic === ''
-								: values.subTopic === ''
-						}
-						sx={{ float: 'right' }}
-					>
-						<div className='submitBtn' id='submitBtn'>
-							送出
-							<Send id='sendIcon' />
-						</div>
-					</Submitbtn>
-				</form>
-			);
-	}
+        default:
+            return (
+                <form onSubmit={handleSubmit}>
+                    <Box sx={{ my: { xs: 3 } }}>
+                        <FormControl fullWidth>
+                            <TextField
+                                variant="filled"
+                                label="學號"
+                                name="studentId"
+                                color={
+                                    StudentIdValidator(values.studentId)
+                                        ? 'success'
+                                        : 'warning'
+                                }
+                                value={values.studentId}
+                                onChange={handleTextChange}
+                            />
+                            {!StudentIdValidator(values.studentId) && (
+                                <FormHelperText>請輸入8碼學號</FormHelperText>
+                            )}
+                        </FormControl>
+                    </Box>
+                    {StudentIdValidator(values.studentId) && (
+                        <Box sx={{ my: { xs: 3 } }}>
+                            <FormControl fullWidth>
+                                <InputLabel>主題 *</InputLabel>
+                                <Select
+                                    label="主題 *"
+                                    name="mainTopic"
+                                    options={mainTopics}
+                                    value={values.mainTopic}
+                                    onChange={handleTopicChange}
+                                />
+                            </FormControl>
+                        </Box>
+                    )}
+                    {values.mainTopic !== '' && values.mainTopic !== 8 && (
+                        <Box sx={{ my: { xs: 3 } }}>
+                            <FormControl fullWidth>
+                                <InputLabel>副主題 *</InputLabel>
+                                <Select
+                                    label="副主題 *"
+                                    name="subTopic"
+                                    options={subTopics[values.mainTopic]}
+                                    value={values.subTopic}
+                                    onChange={handleSubTopicChange}
+                                />
+                            </FormControl>
+                        </Box>
+                    )}
+                    {values.mainTopic === 8 && (
+                        <Box sx={{ my: { xs: 3 } }}>
+                            <FormControl fullWidth>
+                                <TextField
+                                    variant="filled"
+                                    label="其他"
+                                    name="otherTopic"
+                                    value={values.otherTopic}
+                                    onChange={handleTextChange}
+                                />
+                            </FormControl>
+                        </Box>
+                    )}
+                    {console.log(values.members)}
+                    {(values.subTopic !== '' || values.otherTopic !== '') && (
+                        <Box sx={{ my: { xs: 3 } }}>
+                            <FormControl fullWidth>
+                                <InputLabel>組員人數</InputLabel>
+                                <Select
+                                    label="組員人數"
+                                    name="members"
+                                    options={members}
+                                    value={values.members}
+                                    onChange={handleMemberSelect}
+                                />
+                            </FormControl>
+                        </Box>
+                    )}
+                    {values.members !== 1 && <>{memberInput(values.members)}</>}
+                    <IconButton onClick={handleDelete}>
+                        <DeleteOutline />
+                    </IconButton>
+                    <Submitbtn
+                        type="submit"
+                        disableRipple
+                        disabled={
+                            values.mainTopic === 8
+                                ? values.otherTopic === ''
+                                : values.subTopic === '' ||
+                                  values.members === '' ||
+                                  (values.members === 2 &&
+                                      values.memberInfo1 === '') ||
+                                  (values.members === 3 &&
+                                      values.memberInfo1 === '') ||
+                                  values.memberInfo2 === ''
+                        }
+                        sx={{ float: 'right' }}
+                    >
+                        <div className="submitBtn" id="submitBtn">
+                            送出
+                            <Send id="sendIcon" />
+                        </div>
+                    </Submitbtn>
+                </form>
+            );
+    }
 };
 
 export default Form;
