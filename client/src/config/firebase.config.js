65055ec1fbf86b9ef8441e03aca4f079c886749e
LK152 <wuluke0@gmail.com>
2021-12-20 16:20:37 +0800

    sas

--- client/src/config/firebase.config.js
+++ client/src/config/firebase.config.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
   
 const firebaseConfig = {
-  apiKey: "AIzaSyBMb2QgttdwaC-zwizPKblfa6OjqT_YoBo",
-  authDomain: "schooldev-66db8.firebaseapp.com",
-  projectId: "schooldev-66db8",
-  storageBucket: "schooldev-66db8.appspot.com",
-  messagingSenderId: "127427728758",
-  appId: "1:127427728758:web:d049746de51a4e1bf9d657",
-  measurementId: "G-K6GFT6P55B"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_DOMAIN,
+  projectId: process.env.REACT_APP_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASURE_ID
 };
 
 const app = initializeApp(firebaseConfig);
