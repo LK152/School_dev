1aa3090a7f4b92becfb9f00823e8f0f701d38f67
LK152 <wuluke0@gmail.com>
2022-01-08 10:48:20 +0800

    lol

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
 import './App.css';
 import { useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useMediaQuery } from '@mui/material';
 import Home from './components/Home';
 import Form from './components/Form';
 import Results from './components/Results';
 import Navbar from './components/Navbar';
 import Login from './components/Login';
 import Edit from './components/Edit';
+import MobileNavbar from './mobile/components/MobileNavbar';
 import { ModalContext } from './context/ModalContext';
 
 const App = () => {
 	const { infoObj } = useContext(ModalContext);
 	const [info] = infoObj;
+	const isBrowser = Boolean(useMediaQuery('(min-width: 441px)'));
 
 	return (
 		<Router>
-			<Navbar />
+			{isBrowser ? <Navbar /> : <MobileNavbar />}
 			<Routes>
 				<Route exact path='/' element={info ? <Home /> : <Login />} />
 				<Route
