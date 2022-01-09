ed46a3f71ef00db5e892d747e4493e33728a003d
steel201925 <a09080425@gmail.com>
2022-01-09 22:13:48 +0800

    123

---
+++ client/src/components/Back.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+import {
+    IconButton,
+    Container,
+    Card,
+    CardContent,
+    Typography,
+    Grid,
+} from '@mui/material';
+import { Edit, DeleteForever } from '@mui/icons-material';
+import { db } from '../service/firestore';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import { Link } from 'react-router-dom';
+import CellEditServerSidePersistence from './Test_edit'
+
+const back = () => {
+    return (
+        1234546
+        <Container>
+            <CellEditServerSidePersistence ></CellEditServerSidePersistence>
+        </Container>
+    );
+};
+
+export default back;
