8aa498c179dd7fdaa1be1fd1fe8088f7b64757e8
steel201925 <a09080425@gmail.com>
2022-01-29 16:11:42 +0800

    1234564564878784542343

---
+++ client/components/TeacherTable.jsx
@@ -0,0 +1,142 @@
+import { useState } from 'react';
+import {
+	DataGrid,
+	GridToolbarContainer,
+	GridToolbarDensitySelector,
+} from '@mui/x-data-grid';
+import { Button } from '@mui/material';
+import { Download } from '@mui/icons-material';
+import exportXL from './api/exportXL';
+import useStateContext from '../src/context/StateContext';
+
+const columns = [
+	{
+		field: 'id',
+		headerName: '學生Email',
+		width: 200,
+	},
+	{
+		field: 'classNumber',
+		headerName: '班級座號',
+		width: 100,
+	},
+	{
+		field: 'name',
+		headerName: '姓名',
+		width: 80,
+	},
+	{
+		field: 'topic',
+		headerName: '主題',
+		width: 120,
+	},
+	{
+		field: 'subTopic',
+		headerName: '副主題',
+		width: 120,
+	},
+	{
+		field: 'comment',
+		headerName: '備註',
+		width: 160,
+	},
+	{
+		field: 'memNum',
+		headerName: '組員人數',
+		width: 100,
+	},
+	{
+		field: 'mem1',
+		headerName: '組員1',
+		width: 80,
+	},
+	{
+		field: 'mem2',
+		headerName: '組員2',
+		width: 80,
+	},
+	{
+		field: 'group',
+		headerName: '分組',
+		width: 100,
+	},
+];
+
+const TeacherTable = () => {
+	const [pageSize, setPageSize] = useState(50);
+	const { studentRecord, authState, selected, setSelected } = useStateContext();
+	const { isAdmin, teacherClass } = authState;
+
+	const handleExport = () => {
+		exportXL(studentRecord, '自主學習');
+	};
+
+	const CustomToolbar = () => {
+		return (
+			<GridToolbarContainer
+				sx={{ display: 'flex', justifyContent: 'space-between' }}
+			>
+				<GridToolbarDensitySelector size='medium' />
+				<Button onClick={handleExport}>
+					<Download />
+					匯出
+				</Button>
+			</GridToolbarContainer>
+		);
+	};
+
+	const handleClass = (record) => {
+		return record.group === teacherClass;
+	}
+
+	const studentRecords = studentRecord.filter(handleClass);
+
+	const rows = studentRecords.map((doc) => {
+		return {
+			id: doc.email,
+			name: doc.studentName,
+			classNumber:
+				doc.studentClass.toString() +
+				(doc.number < 10
+					? '0' + doc.number.toString()
+					: doc.number.toString()),
+			topic: doc.topicLabel,
+			subTopic: doc.subTopicLabel,
+			comment: doc.comment !== '' ? doc.comment : 'N/A',
+			memNum: doc.memNum,
+			mem1:
+				doc.mem1Class !== '' && doc.mem1Num !== ''
+					? doc.mem1Class.toString() +
+					  (doc.mem1Num < 10
+							? '0' + doc.mem1Num.toString()
+							: doc.mem1Num.toString())
+					: 'N/A',
+			mem2:
+				doc.mem2Class !== '' && doc.mem2Num !== ''
+					? doc.mem2Class.toString() +
+					  (doc.mem2Num < 10
+							? '0' + doc.mem2Num.toString()
+							: doc.mem2Num.toString())
+					: 'N/A',
+			group: doc.group,
+		};
+	});
+
+	return (
+		<div style={{ height: '50vh', width: '100%' }}>
+			<DataGrid
+				rows={rows}
+				columns={columns}
+				pageSize={pageSize}
+				onPageSizeChange={(newPS) => setPageSize(newPS)}
+				rowsPerPageOptions={[10, 25, 50, 100]}
+				components={{ Toolbar: isAdmin && CustomToolbar }}
+				disableColumnFilter
+				disableColumnMenu
+				disableSelectionOnClick
+			/>
+		</div>
+	);
+};
+
+export default TeacherTable;
--- client/pages/Grouping.js
+++ client/pages/Grouping.js
@@ -1,125 +1,128 @@
 import { withUser } from '../src/hook/route';
 import {
-	Container,
-	Card,
-	CardContent,
-	Grid,
-	Typography,
-	FormControl,
-	Button,
+    Container,
+    Card,
+    CardContent,
+    Grid,
+    Typography,
+    FormControl,
+    Button,
 } from '@mui/material';
 import Select from '../components/Select';
 import StudentTable from '../components/StudentTable';
+import TeacherTable from '../components/TeacherTable';
 import { teachers } from '../data/Option';
 import axios from 'axios';
 import useStateContext from '../src/context/StateContext';
 
 const Grouping = () => {
-	const { authState, selectedValues, setSelectedValues, selected } =
-		useStateContext();
-	const { isAdmin } = authState;
-	const { selectedGroup } = selectedValues;
+    const { authState, selectedValues, setSelectedValues, selected } =
+        useStateContext();
+    const { isAdmin, isTeacher } = authState;
+    const { selectedGroup } = selectedValues;
 
-	const [teacher] = teachers.filter((res) => {
-		return res.value === selectedGroup;
-	});
+    const [teacher] = teachers.filter((res) => {
+        return res.value === selectedGroup;
+    });
 
-	const handleSelect = (e) => {
-		setSelectedValues({
-			...selectedValues,
-			[e.target.name]: e.target.value,
-		});
-	};
+    const handleSelect = (e) => {
+        setSelectedValues({
+            ...selectedValues,
+            [e.target.name]: e.target.value,
+        });
+    };
 
-	const handleUpdate = async () => {
-		const data = {
-			selected: selected,
-			group: teacher.label,
-		};
+    const handleUpdate = async () => {
+        const data = {
+            selected: selected,
+            group: teacher.label,
+        };
 
-		await axios.patch('/api/admin/group', data);
-	};
+        await axios.patch('/api/admin/group', data);
+    };
 
-	const handleDelete = async () => {
-		await axios.post('/api/admin/group', selected);
-	};
+    const handleDelete = async () => {
+        await axios.post('/api/admin/group', selected);
+    };
 
-	return (
-		<Container sx={{ my: 10 }}>
-			<Card raised>
-				<CardContent>
-					<Grid container direction='column' spacing={2}>
-						<Grid item>
-							<Typography variant='h3' textAlign='center'>
-								學生分組
-							</Typography>
-						</Grid>
-						<Grid item>
-							<StudentTable handleSelect={handleSelect} />
-						</Grid>
-						{isAdmin && (
-							<Grid
-								item
-								container
-								direction='row'
-								alignItems='center'
-								columnSpacing={2}
-							>
-								<Grid item>
-									<Typography variant='h6' sx={{ ml: 2 }}>
-										分配組別
-									</Typography>
-								</Grid>
-								<Grid item sm={4} xs={8}>
-									<FormControl fullWidth>
-										<Select
-											name='selectedGroup'
-											value={selectedGroup}
-											options={teachers}
-											onChange={handleSelect}
-											sx={{ ml: 10 }}
-										/>
-									</FormControl>
-								</Grid>
-								<div style={{ flexGrow: 1 }} />
-								<Grid
-									item
-									container
-									direction='row'
-									justifyContent='space-between'
-								>
-									<Grid item>
-										<Button
-											variant='contained'
-											disabled={selected.length === 0}
-											onClick={handleDelete}
-											sx={{ margin: 2 }}
-										>
-											<Typography color='common.white'>
-												刪除
-											</Typography>
-										</Button>
-									</Grid>
-									<Grid item>
-										<Button
-											variant='contained'
-											disabled={selected.length === 0}
-											onClick={handleUpdate}
-											sx={{ margin: 2 }}
-										>
-											<Typography color='common.white'>
-												新增
-											</Typography>
-										</Button>
-									</Grid>
-								</Grid>
-							</Grid>
-						)}
-					</Grid>
-				</CardContent>
-			</Card>
-		</Container>
-	);
+    return (
+        <Container sx={{ my: 10 }}>
+            <Card raised>
+                <CardContent>
+                    <Grid container direction="column" spacing={2}>
+                        <Grid item>
+                            <Typography variant="h3" textAlign="center">
+                                學生分組
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <StudentTable handleSelect={handleSelect} />
+                        </Grid>
+                        {isAdmin && !isTeacher ? (
+                            <Grid
+                                item
+                                container
+                                direction="row"
+                                alignItems="center"
+                                columnSpacing={2}
+                            >
+                                <Grid item>
+                                    <Typography variant="h6" sx={{ ml: 2 }}>
+                                        分配組別
+                                    </Typography>
+                                </Grid>
+                                <Grid item sm={4} xs={8}>
+                                    <FormControl fullWidth>
+                                        <Select
+                                            name="selectedGroup"
+                                            value={selectedGroup}
+                                            options={teachers}
+                                            onChange={handleSelect}
+                                            sx={{ ml: 10 }}
+                                        />
+                                    </FormControl>
+                                </Grid>
+                                <div style={{ flexGrow: 1 }} />
+                                <Grid
+                                    item
+                                    container
+                                    direction="row"
+                                    justifyContent="space-between"
+                                >
+                                    <Grid item>
+                                        <Button
+                                            variant="contained"
+                                            disabled={selected.length === 0}
+                                            onClick={handleDelete}
+                                            sx={{ margin: 2 }}
+                                        >
+                                            <Typography color="common.white">
+                                                刪除
+                                            </Typography>
+                                        </Button>
+                                    </Grid>
+                                    <Grid item>
+                                        <Button
+                                            variant="contained"
+                                            disabled={selected.length === 0}
+                                            onClick={handleUpdate}
+                                            sx={{ margin: 2 }}
+                                        >
+                                            <Typography color="common.white">
+                                                新增
+                                            </Typography>
+                                        </Button>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        ) : (
+                            <TeacherTable />
+                        )}
+                    </Grid>
+                </CardContent>
+            </Card>
+        </Container>
+    );
 };
 
 export default withUser(Grouping);
