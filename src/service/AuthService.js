cbe86779e81c3c92bb4d0267ee08f5bf0c64ebf1
LK152 <wuluke0@gmail.com>
2022-01-31 19:00:18 +0800

    s

--- src/service/AuthService.js
+++ src/service/AuthService.js
@@ -5,15 +5,11 @@ import {
 	signOut,
 	getAuth,
 } from 'firebase/auth';
-import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
 import { app } from '../config/firebase.config';
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-enableIndexedDbPersistence(db).catch((err) => {
-	console.log(err);
-});
 
 export const AuthService = {
 	loginWithGoogleBrowser: async () => {
