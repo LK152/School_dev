4d5dc583748e5f670b24a3f9baa203ac796de45b
LK152 <wuluke0@gmail.com>
2022-02-02 23:32:28 +0800

    api validation

--- pages/api/admin/group/index.js
+++ pages/api/admin/group/index.js
@@ -1,46 +1,61 @@
 import admin from '../../../../utils/db';
+import initMiddleware from '@src/lib/initMiddleware';
+import validateMiddleware from '@src/lib/validateMiddleware';
+import { body, validationResult } from 'express-validator';
+
+const validateAdd = initMiddleware(
+	validateMiddleware(
+		[body('selected').isLength({ min: 1, max: 100 })],
+		validationResult
+	)
+);
+
+const validateDelete = initMiddleware(
+	validateMiddleware(
+		[body().isLength({ min: 1, max: 100 })],
+		validationResult
+	)
+);
 
 // eslint-disable-next-line
 export default async (req, res) => {
+	const errors = validationResult(req);
+
 	try {
 		switch (req.method) {
 			case 'PATCH':
-				if (
-					Array.isArray(req.body.selected) &&
-					req.body.selected.length <= 100
-				) {
-					await req.body.selected.forEach((id) => {
-						admin
-							.firestore()
-							.collection('studentData')
-							.doc(id)
-							.update({
-								group: req.body.group,
-								groupClass: req.body.groupClass,
-							});
-					});
+				await validateAdd(req, res);
 
-					return res.status(201).end();
-				} else {
-					return res.status(406);
+				if (!errors.isEmpty()) {
+					return res.status(422).json({ errors: errors.array() });
 				}
+				await req.body.selected.forEach((id) => {
+					admin.firestore().collection('studentData').doc(id).update({
+						group: req.body.group,
+						groupClass: req.body.groupClass,
+					});
+				});
+
+				return res.status(201).end();
 
 			case 'POST':
-				if (Array.isArray(req.body) && req.body.length <= 100) {
-					await req.body.forEach((id) => {
-						admin
-							.firestore()
-							.collection('studentData')
-							.doc(id)
-							.update({ group: '', groupClass: '' })
-							.then(() => res.status(200).end())
-							.catch(() => res.status(400).end());
-					});
+				await validateDelete(req, res);
 
-					res.status(200).end();
-				} else {
-					res.status(406).end();
+				if (!errors.isEmpty()) {
+					return res.status(422).json({ errors: errors.array() });
 				}
+
+				await req.body.forEach((id) => {
+					admin
+						.firestore()
+						.collection('studentData')
+						.doc(id)
+						.update({ group: '', groupClass: '' })
+						.then(() => res.status(200).end())
+						.catch(() => res.status(400).end());
+				});
+
+				res.status(200).end();
 		}
 	} catch (err) {
 		res.status(400).end();
