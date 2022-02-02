fdc57b7e47cea57a146999073245bdda157d5072
LK152 <wuluke0@gmail.com>
2022-02-02 23:43:46 +0800

    f

--- next.config.js
+++ next.config.js
@@ -2,10 +2,4 @@ module.exports = {
 	reactStrictMode: true,
 	poweredByHeader: false,
 	staticPageGenerationTimeout: 10000,
-	images: {
-		domains: [
-			'https://school-dev.netlify.app',
-			'https://school-dev.vercel.app',
-		],
-	},
 };
