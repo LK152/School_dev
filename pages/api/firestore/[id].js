eb9a74aeef1c4fb641330513ecf4d2751981f873
LK152 <wuluke0@gmail.com>
2022-03-03 00:07:46 +0800

    perf

--- pages/api/firestore/[id].js
+++ pages/api/firestore/[id].js
@@ -2,6 +2,19 @@ import admin from '../../../utils/db';
 
 const studentDB = admin.firestore().collection('studentData');
 
+const getDate = () => {
+	const dt = new Date();
+	const year = dt.getFullYear();
+	const month = '0' + (dt.getMonth() + 1);
+	const date = '0' + dt.getDate();
+	const hours = dt.getHours();
+	const minutes = dt.getMinutes();
+	const seconds = dt.getSeconds();
+	const output = `${year}-${month}-${date}-${hours}-${minutes}-${seconds}`;
+
+	return output;
+};
+
 // eslint-disable-next-line
 export default async (req, res) => {
 	const { id } = req.query;
@@ -9,7 +22,12 @@ export default async (req, res) => {
 	try {
 		switch (req.method) {
 			case 'POST':
-				await studentDB.doc(id).set({ ...req.body }, { merge: true });
+				await studentDB
+					.doc(id)
+					.set(
+						{ ...req.body, dateCreated: getDate() },
+						{ merge: true }
+					);
 				return res.status(201).end();
 
 			case 'DELETE':
